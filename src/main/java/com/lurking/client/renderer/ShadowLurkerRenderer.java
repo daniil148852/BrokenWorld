0077705acbf8a0194451f3b1bbea684f02160eb7
daniil148852 <daniildan850@gmail.com>
2026-04-29 20:33:02 +0200

    Update ShadowLurkerRenderer.java

--- src/main/java/com/lurking/client/renderer/ShadowLurkerRenderer.java
+++ src/main/java/com/lurking/client/renderer/ShadowLurkerRenderer.java
@@ -4,16 +4,16 @@ import com.lurking.TheLurking;
 import com.lurking.entity.ShadowLurkerEntity;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.entity.model.ZombieEntityModel;
 import net.minecraft.util.Identifier;
 
-public class ShadowLurkerRenderer extends MobEntityRenderer<ShadowLurkerEntity, ZombieEntityModel<ShadowLurkerEntity>> {
+public class ShadowLurkerRenderer extends MobEntityRenderer<ShadowLurkerEntity, BipedEntityModel<ShadowLurkerEntity>> {
     
     private static final Identifier TEXTURE = new Identifier(TheLurking.MOD_ID, "textures/entity/shadow_lurker.png");
 
     public ShadowLurkerRenderer(EntityRendererFactory.Context context) {
-        super(context, new ZombieEntityModel<>(context.getPart(EntityModelLayers.ZOMBIE)), 0.5f);
+        super(context, new BipedEntityModel<>(context.getPart(EntityModelLayers.PLAYER)), 0.5f);
     }
 
     @Override
